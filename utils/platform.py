d7a720b863d04adaa6465120bb5f2745141e59a4
ZZZ <zxx457@ZZZs-MacBook-Pro.local>
2025-05-15 10:14:02 +0800

    add macos suppport with brew installer

--- utils/platform.py
+++ utils/platform.py
@@ -1,17 +1,22 @@
 import sys
 from enum import Enum 
+import shutil
 
-class Platform(Enum):
+def is_program_installed(program_name):
+    return shutil.which(program_name) is not None
+
+class PLATFORM(Enum):
     MAC = 0
     LINUX = 1
     WINDOWS = 2
 
-def detect_platform() -> Platform:
+def detect_platform() -> PLATFORM:
     
     if sys.platform.startswith('win'):
-        return Platform.WINDOWS
+        return PLATFORM.WINDOWS
     elif sys.platform.startswith('linux'):
-        return Platform.LINUX
+        return PLATFORM.LINUX
     elif sys.platform.startswith('darwin'):
-        return Platform.MAC
+        return PLATFORM.MAC
     
+
